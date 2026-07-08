bdede1e7db632e66f9bea98ad23731187c0518f4
RAMA <RAMA@DESKTOP-0GV43IO>
2026-07-08 10:19:11 +0700

    Update Script Refresh

--- Scripts/BuatRefresh/Script1783150826973.groovy
+++ Scripts/BuatRefresh/Script1783150826973.groovy
@@ -17,6 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.delay(6)
+WebUI.delay(8)
 WebUI.refresh()
-WebUI.delay(6)
+WebUI.delay(8)
