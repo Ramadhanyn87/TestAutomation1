ce0eac616124843b666fea663a4d59864999e4b4
RAMA <RAMA@DESKTOP-0GV43IO>
2026-08-06 21:59:49 +0700

    Benerin Object

--- Object Repository/2.Objectautomationexercise/BtCartCO/12. InptExpiryYears.rs
+++ Object Repository/2.Objectautomationexercise/BtCartCO/12. InptExpiryYears.rs
@@ -6,12 +6,11 @@
    <elementGuidId>5c1fc30a-7bc8-4239-9990-fb36f38aa649</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
-         <value></value>
+         <key>XPATH</key>
+         <value>//input[@data-qa='expiry-year']</value>
       </entry>
       <entry>
-         <key>XPATH</key>
-         <value>//input[@data-qa='expiry-month']</value>
+         <key>BASIC</key>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
